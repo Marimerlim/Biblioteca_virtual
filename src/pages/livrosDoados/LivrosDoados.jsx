c6eae355dd7cf8432a7aa723551b75e9f984c8f5
Marimerlim <mariri.merlim@gmail.com>
2024-11-29 20:34:55 -0300

    Projeto finalizado e responsivo

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,7 +1,20 @@
+import Livro from '../../assets/OProtagonista.png'
+import s from './livrosDoados.module.scss'
+
 export default function LivrosDoados() {
     return (
-      <section>
-        <h2>Aqui estão os livros doados</h2>
+      <section className={s.pageL}>
+        <h2>Estante</h2>
+        <section className={s.containerCards}>
+          <section>
+            <img src={Livro} alt="Capa do Livro O Protagonista de Sussane Andrade" />
+            <div>
+            <h3>O protagonista</h3>
+            <p>Susanne Andrade</p>
+            <p>Ficção</p>
+            </div>
+          </section>
+        </section>
       </section>
     );
   }
